1a395c1d61c6fdeec8917bc7b680db2ce54f2d3d
JoyceQwQ <47224705+JoyceQwQ@users.noreply.github.com>
2022-03-03 00:04:38 +0800

    Update Detector.py

--- Detector/Detector.py
+++ Detector/Detector.py
@@ -14,7 +14,7 @@ class Detector:
             self.model = torch.hub.load('ultralytics/yolov5', model)  # or yolov5m, yolov5l, yolov5x, etc.
         except:
             print("Error in Detecter: fail to load model")
-        Detector.id = 0
+        Detector.id = 1
 
     #return list of records
     def scanImg(self,

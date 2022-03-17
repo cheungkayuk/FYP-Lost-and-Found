86e53b0f52a3a9a3ae598fa3432eca1765275693
cheungkayuk <cheungkayuk121@gmail.com>
2022-03-17 17:07:02 +0800

    change function format, add default value

--- Similarity/Similarity.py
+++ Similarity/Similarity.py
@@ -10,7 +10,16 @@ class Similarity:
         sim = 1 - (hash1 - hash2) / len(hash1.hash) ** 2
         return sim
 
-    def similarity (self, image1, image2, method, threshold1, threshold2, threshold3):
+    def similarity (self, image1, image2, method = "dhash", threshold=None):
+        threshold1 = 0.85
+        threshold2 = 0.8
+        threshold3 = 0.8
+        if threshold:
+            if threshold>=0 and threshold<=1:
+                threshold1 = threshold
+                threshold2 = threshold
+                threshold3 = threshold
+
 
         hash_size = 8
         mode = 'db4'
@@ -25,8 +34,8 @@ class Similarity:
             image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
             s = ssim(image1, image2, multichannel=True)
             if s > threshold1:
-                return True
-            return False
+                return True,s
+            return False,s
 
         elif method == "dhash":
             image1 = Image.open(image1)
@@ -35,8 +44,8 @@ class Similarity:
             dhash2 = imagehash.dhash(image2, hash_size = hash_size)
             s = self.cal_similar(dhash1, dhash2)
             if s > threshold2:
-                return True
-            return False
+                return True,s
+            return False,s
 
         elif method == "whash":
             image1 = Image.open(image1)
@@ -45,13 +54,14 @@ class Similarity:
             whash2 = imagehash.whash(image2, image_scale = image_scale, hash_size = hash_size, mode = mode)
             s = self.cal_similar(whash1, whash2)
             if s > threshold3:
-                return True
-            return False
+                return True,s
+            return False,s
+
+        if method == "all":
+            pass
 
 # from Similarity import Similarity
 # image1 = "crop1.png"
 # image2 = "crop2.png"
-# # image1 = cv2.imread(image1)
-# # image2 = cv2.imread(image2)
 # s = Similarity()
-# s.similarity(image1, image2, method="ssim", threshold1=0.8, threshold2=0.6, threshold3=0.6)
+# s.similarity(image1, image2, method="ssim")

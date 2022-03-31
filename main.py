b4bdc0bc5193d7273470570b71cec0d25ffb9ed0
cheungkayuk <cheungkayuk121@gmail.com>
2022-03-31 15:33:56 +0800

    correct the point name

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ import time
 from time import sleep
 
 ###################################################################################
-A0 = ["A1",(3.558732032775879, 6.074324131011963, 0.977596640586853, 0.21048696339130402)]
-A1 = ["A0",(3.558732032775879, 6.074324131011963, -0.7992277145385742, 0.6010283827781677)]
+A0 = ["A0",(3.558732032775879, 6.074324131011963, 0.977596640586853, 0.21048696339130402)]
+A1 = ["A1",(3.558732032775879, 6.074324131011963, -0.7992277145385742, 0.6010283827781677)]
 B0 = ["B0",(6.456136703491211, 5.444826602935791, -0.08248760551214218, 0.996592104434967)]
 B1 = ["B1",(6.456136703491211, 5.444826602935791, -0.8250911235809326, 0.5649996995925903)]
 # B0 = ["B1",(11.809239387512207, 4.119314670562744, -0.13229386508464813, 0.9912105202674866)]
@@ -45,7 +45,7 @@ class LAF_Robot():
         self._scan_road = 0
 
         self.mode = 0   # 0: nothing    1: 1st class    2: 2nd class
-        self.t = threading.Thread(target=self._scan_road)
+        self.t = threading.Thread(target=self.t_scan_road)
         self.enable_t = enable_t
 
         self.keep_patrol = True
@@ -100,17 +100,19 @@ class LAF_Robot():
         loop = asyncio.get_event_loop()
 
         while self.enable_t: 
-            if (self.robotcontroller.moving) and (self.mode == 1):
+            # if (self.robotcontroller.moving) and (self.mode == 1):
+            if True:
                 # SCAN ROAD
+                print("im working\n")
                 sleep(2)
-                oblist = self.detecter.scanImg(self.camera.getimg(), mode = 1)
+                oblist = self.detector.scanImg(self.camera.getimg(), mode = 1)
                 if len(oblist):
                     print("\nREPORT!!!!!")
                     print(oblist)
                     print("End!!!!!\n")
                     pt = self.cur_pt()
                     for item in oblist:
-                        self.alerter.sendToLog(self, item["full_img"], item["name"], pt[0][0], pt[0][1], item["time"])
+                        self.alerter.sendToLog(item["full_img"], item["name"], pt[0][0], pt[0][1], item["time"])
                     sleep(10)
 
             else:
@@ -134,10 +136,17 @@ class LAF_Robot():
         self.dbcontroller.createNewCheckpoint("B", B0[1][0], B0[1][1], [(B0[1][2],B0[1][3]),(B1[1][2],B1[1][3])])
         
 
-# robot = LAF_Robot()
-# robot.alerter.sendToLog("imgFileName", "className", "checkpoint", "direction", "detectedTime")
-# robot.start_patrol()
+#robot = LAF_Robot()
+#robot.alerter.sendToLog("imgFileName", "className", "checkpoint", "direction", "detectedTime")
+#robot.start_patrol()
 # robot.init_database()
+
 # robot.t_start_scan_road()
+# stop = input("press to stop")
+# robot.enable_t = False
 # a = Alerter(internet=True)
 # a.readAndSendFromLog()
+#robot.robotcontroller.goto(A0[1])
+#robot.robotcontroller.goto(A1[1])
+#robot.robotcontroller.goto(B0[1])
+#robot.robotcontroller.goto(B1[1])

a301c4a3f0c00ef2a1f37bcc3b3f93352c87b0d2
JoyceQwQ <47224705+JoyceQwQ@users.noreply.github.com>
2022-04-18 14:15:18 +0800

    Add documentation

--- Data Preparation/Objects365/drawAnnotations_Objects365.py
+++ Data Preparation/Objects365/drawAnnotations_Objects365.py
@@ -2,18 +2,23 @@ import json
 import os
 import cv2
 
-
+# json filename
 json_file = "image_annotation_Telephone.json"
 
+# patch folder (Objects365 groups images into patches)
 patchFolder = 'patch0'
 
 colour = (255, 0, 0)
 thickness = 1
 
+# open json file
 f = open(json_file)
 
+# load json file
 data = json.load(f)
 
+# Draw the bounding boxes loaded from the annotation file on the image
+# Save the drawn image
 for image_id in data:
     image = data.get(image_id)
     if(image['file_name']):

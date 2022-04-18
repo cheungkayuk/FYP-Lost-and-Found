adeecaf2f8e3d646fdc29963e5704bfaa8c0db7f
JoyceQwQ <47224705+JoyceQwQ@users.noreply.github.com>
2022-04-18 14:18:29 +0800

    Add documentation

--- Data Preparation/Objects365/extract_Objects365.py
+++ Data Preparation/Objects365/extract_Objects365.py
@@ -5,24 +5,31 @@ import cv2
 # json_file = "Objects365\Train\zhiyuan_objv2_train.json"
 # MemoryError
 
+# json filename
 json_file = "Objects365\Val\zhiyuan_objv2_val.json"
 
+# open the json file
 f = open(json_file)
 
+# load the json file
 data = json.load(f)
 
+# target class list for filtering
 classlist = ['Handbag/Satchel', 'Backpack', 'Cell Phone', 'Luggage', 'Wallet/Purse']
 
 # classlist = ['Telephone']
 
+# get the class id of the target classes
 classlist_ids = []
 for category in data["categories"]:
     if category['name'] in classlist:
         print(category)
         classlist_ids.append(category['id'])
 
+# the name of json file to be created
 image_annotation_json = 'image_annotation.json'
 
+# extract the filtered annotations from the loaded json
 image_ann_id = {}
 for annotation in data['annotations']:
     if annotation['category_id'] in classlist_ids:
